fd595caca5aa3db741456d894156053548c9f344
ganeshmiriyam <57966812+ganeshmiriyam@users.noreply.github.com>
2026-08-20 09:42:20 -0400

    Add Docker and CI pipeline

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from app.routes.products import router as product_router
+from app.database import engine, Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="ShopSphere API",
+    version="1.0.0"
+)
 
 app = FastAPI(
     title="ShopSphere API",
